679fd98e6be44f1890158777173f453284d667c9
José Manuel Nieto <superjmn@outlook.com>
2023-02-02 23:57:37 +0100

    Add project files.

---
+++ AvaloniaApplication8/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+using System.Collections.ObjectModel;
+using System.Reactive;
+using ReactiveUI;
+
+namespace AvaloniaApplication8.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        Items = new ObservableCollection<Item>(new[]
+        {
+            new Item { Name = "Item One" }, 
+            new Item { Name = "Item TWO" }
+        });
+
+        MoveToTop = ReactiveCommand.Create(() => Items.Move(1, 0));
+    }
+
+    public ReactiveCommand<Unit, Unit> MoveToTop { get; }
+
+    public ObservableCollection<Item> Items { get; set; }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
